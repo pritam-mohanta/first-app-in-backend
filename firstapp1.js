927e42ec3f646cf1c4e98ed0a0dff185e5f2cf88
pritam-mohanta <samratmohanta7001@gmail.com>
2024-01-20 00:25:52 +0530

    1st req project

--- firstapp1.js
+++ firstapp1.js
@@ -3,8 +3,51 @@
 const http = require('http') // export module http
 
 const server = http.createServer((req,res)=>{    //create a new server
-  console.log("Pritam")
-  console.log(req)
+  //console.log("Pritam")
+  //console.log(req.url,req.method,req.headers)
+
+
+  //way to get response
+  // res.setHeader('content-type','text/html')
+  // res.write('<html>')
+  // res.write('<title>First response</title>')
+  // res.write('<body><h1>Pritam Mohanta</h1></body>')
+  // res.write('</html>')
+
+  const url = req.url
+
+  if(url === '/home'){
+    res.write('<html>')
+    res.write('<title>home</title>')
+    res.write('<body><h1>Welcome home</h1></body>')
+    res.write('</html>')
+    return res.end()
+  }
+
+  if(url === '/about'){
+    res.write('<html>')
+    res.write('<title>about</title>')
+    res.write('<body><h1>Welcome to About Us page</h1></body>')
+    res.write('</html>')
+    return res.end()
+  }
+
+  if(url === '/node'){
+    res.write('<html>')
+    res.write('<title>node</title>')
+    res.write('<body><h1>....Welcome to my Node Js project....</h1></body>')
+    res.write('</html>')
+    return res.end()
+  }
+
+  res.write('<html>')
+  res.write('<title>First response</title>')
+  res.write('<body><h1>Welcome to my Node Js project</h1></body>')
+  res.write('</html>')
+  res.end()
+
+
+  
 })
 
 server.listen(4000)//the port this gone run
