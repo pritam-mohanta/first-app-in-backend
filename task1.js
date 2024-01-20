6c48005afb555b140759784849c8767f08335fd8
pritam-mohanta <samratmohanta7001@gmail.com>
2024-01-20 20:42:16 +0530

    first task of Backend

---
+++ task1.js
@@ -0,0 +1,56 @@
+
+const http = require('http')
+const fs = require('fs') // export module http
+
+const server = http.createServer((req,res)=>{    //create a new server
+
+  const url = req.url
+  const method = req.method
+
+  if(url==='/'){
+
+    fs.readFile('message.txt',{encoding:'utf-8'},(err,data)=>{
+      if(err){
+        console.log(err)
+      }
+      console.log(`data from file`+ data)
+      res.write('<html>')
+    res.write('<title>Enter Massage</title>')
+    res.write(`<body>${data}</body>`)
+    res.write('<body><form action="/massage" method="POST"><input type="text" name="massage"><button type="submit">Send</button></form></body>')
+    res.write('</html>')
+    return res.end()
+    })
+
+  }
+
+  else if(url==='/massage' && method ==='POST'){
+    const body = []
+    req.on('data',(chunk)=>{
+      console.log(chunk)//break the big data into small small part
+      body.push(chunk) //push the data to body and hold it there
+    })
+    req.on('end',()=>{
+      const parseBody = Buffer.concat(body).toString()
+      console.log('ParseBody>>>',parseBody) //it is the massage we write over there
+      const massage = parseBody.split('=')[1] //access the message get the data 
+      fs.writeFile('message.txt',massage,(err)=>{  //and get the data and create an new file and store the data
+        res.statusCode = 302 //network status
+        res.setHeader('Location','/')//where relocate the page
+        return res.end()
+      })
+    }) 
+  }
+
+  else{
+    res.setHeader('content-type','text/html')
+  res.write('<html>')
+  res.write('<title>First response</title>')
+  res.write('<body><h1>Pritam Mohanta</h1></body>')
+  res.write('</html>')
+  res.end()
+  }
+
+})
+
+server.listen(4000)//the port this gone run
