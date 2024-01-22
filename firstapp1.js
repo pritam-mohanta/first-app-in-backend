316b016b41ea0a4a284c4a33f91523dd1d4bc5d2
pritam-mohanta <samratmohanta7001@gmail.com>
2024-01-22 23:19:36 +0530

    first express middleware

--- firstapp1.js
+++ firstapp1.js
@@ -1,59 +1,20 @@
 //server throughnode js
-
 const http = require('http') // export module http
 
-const routes = require('./routs')//custom made routed file imported
-const routes2 = require('./routs2')
+const express = require('express') //require express 
 
-console.log(routes2.someText)
+const app = express()
 
-const server = http.createServer(routes2.handler)
+app.use((req,res,next)=>{
+  console.log('first middleware')
+  next()
+})
+
+app.use((req,res,next)=>{
+  console.log('second middleware')
+  res.send('<h1>hello to node js</h1>')
+})
+
+const server = http.createServer(app)
 
 server.listen(4000)//the port this gone run
-
-
-//console.log("Pritam")
-  //console.log(req.url,req.method,req.headers)
-
-
-  //way to get response
-  // res.setHeader('content-type','text/html')
-  // res.write('<html>')
-  // res.write('<title>First response</title>')
-  // res.write('<body><h1>Pritam Mohanta</h1></body>')
-  // res.write('</html>')
-
-
-  //first small project to handle maney route request
-
-  // const url = req.url
-
-  // if(url === '/home'){
-  //   res.write('<html>')
-  //   res.write('<title>home</title>')
-  //   res.write('<body><h1>Welcome home</h1></body>')
-  //   res.write('</html>')
-  //   return res.end()
-  // }
-
-  // if(url === '/about'){
-  //   res.write('<html>')
-  //   res.write('<title>about</title>')
-  //   res.write('<body><h1>Welcome to About Us page</h1></body>')
-  //   res.write('</html>')
-  //   return res.end()
-  // }
-
-  // if(url === '/node'){
-  //   res.write('<html>')
-  //   res.write('<title>node</title>')
-  //   res.write('<body><h1>....Welcome to my Node Js project....</h1></body>')
-  //   res.write('</html>')
-  //   return res.end()
-  // }
-
-  // res.write('<html>')
-  // res.write('<title>First response</title>')
-  // res.write('<body><h1>Welcome to my Node Js project</h1></body>')
-  // res.write('</html>')
-  // res.end()
