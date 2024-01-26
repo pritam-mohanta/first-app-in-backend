a1301d89c4a0127e8bbf54f129800785ff6c1f21
pritam-mohanta <samratmohanta7001@gmail.com>
2024-01-26 21:52:48 +0530

    first use of body parser

--- firstapp1.js
+++ firstapp1.js
@@ -1,20 +1,40 @@
 //server throughnode js
-const http = require('http') // export module http
-
 const express = require('express') //require express 
+const bodyParser = require('body-parser')//require body-parser in code base
 
 const app = express()
 
-app.use((req,res,next)=>{
-  console.log('first middleware')
-  next()
+// app.use((req,res,next)=>{
+//   console.log('first middleware')
+//   next()
+// })     //--> Basic Middleware
+
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use('/add-product',(req,res,next)=>{
+  //console.log('second middleware')
+  res.send('<form action="/product" method="POST">Product Name:<input type="text" name="title">Product size:<input type="number" name="size"><button type="submit">Add product</button></form>')
 })
 
-app.use((req,res,next)=>{
-  console.log('second middleware')
+app.post('/product',(req,res,next)=>{
+  console.log(req.body) //we can get output object in here
+  console.log(req.body.title)
+  console.log(req.body.size) 
+  res.redirect('/')
+})
+
+app.use('/',(req,res,next)=>{
+  //console.log('second middleware')
   res.send('<h1>hello to node js</h1>')
 })
 
-const server = http.createServer(app)
+app.listen(4000)
 
-server.listen(4000)//the port this gone run
+
+
+
+// app.use('/product',(req,res,next)=>{
+//   console.log(req.body) //we can get output object in here
+//   console.log(req.body.title) 
+//   res.redirect('/')
+// }) ---> in here it execute any type of hhtp request or method
